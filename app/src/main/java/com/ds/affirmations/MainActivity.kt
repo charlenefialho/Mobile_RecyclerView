ce5648713c4123ea3551009362325f88d0a81745
charlenefialho <fialhocharlene@gmail.com>
2022-05-11 22:48:32 -0300

    adicionado recyclerView

--- app/src/main/java/com/ds/affirmations/MainActivity.kt
+++ app/src/main/java/com/ds/affirmations/MainActivity.kt
@@ -2,16 +2,18 @@ package com.ds.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.ds.affirmations.data.Datasource
+import com.ds.affirmations.adapter.ItemAdapter
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myDataset = Datasource().loadAffirmations()
 
-        val textView: TextView = findViewById(R.id.textview_teste)
-
-        textView.text = Datasource().loadAffirmations().size.toString()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
     }
 }
